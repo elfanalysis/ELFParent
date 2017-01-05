44f5b347ce39caac04cb936b3abc8e43dcf4c33c
Nandha Kumar <nandhu12195@gmail.com>
2017-01-05 15:47:57 +0530

    Login Added

---
+++ app/src/main/java/com/elf/elfparent/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.elf.elfparent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ELF on 05-01-2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
